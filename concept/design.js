b4fbb07a2c90f061a43ea0e6d8e1444c9d44b5fb
Mamadsho Nazaramonov <mamadsho.nazaramonov@gmail.com>
2020-10-20 04:41:53 +0500

    SkyBlue

--- concept/design.js
+++ concept/design.js
@@ -5,4 +5,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('.slider').addEventListener('touchend',(evt)=>{
         document.querySelector('#vp').style.opacity='100%';
     })
+    
+    let samplePageStyle=window.getComputedStyle(document.querySelector('.page'),null);
+    let w= parseInt(samplePageStyle.width);    
 })
