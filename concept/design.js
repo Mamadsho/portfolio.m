35026287b3144c5024a36dba3c4a12c8b05beba3
Mamadsho Nazaramonov <mamadsho.nazaramonov@gmail.com>
2020-10-24 06:25:32 +0500

    DesignRefinement

--- concept/design.js
+++ concept/design.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
     document.querySelector('.slider').addEventListener('touchmove',(evt)=>{
-        document.querySelector('#vp').style.opacity='90%';
+        document.querySelector('#vp').style.opacity='95%';
     })
     document.querySelector('.slider').addEventListener('touchend',(evt)=>{
         document.querySelector('#vp').style.opacity='100%';
@@ -8,14 +8,15 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     let samplePageStyle=window.getComputedStyle(document.querySelector('.page'),null);
     let contStyle=window.getComputedStyle(document.querySelector('.container'),null);
-    const w= parseInt(samplePageStyle.width);
-    const contTop=contStyle.top;
-    const contLeft=contStyle.marginLeft;
+    let aPgStyle=window.getComputedStyle(document.querySelector('.activePage'),null);
+    const offsetWidth=2;
+    const w= parseInt(samplePageStyle.width)+2*parseInt(samplePageStyle.marginLeft)+offsetWidth*2;
+    const contTop=parseInt(contStyle.top)-parseInt(aPgStyle.borderTopWidth)-offsetWidth;
+    const contLeft=parseInt(contStyle.marginLeft)-parseInt(aPgStyle.borderLeftWidth)-offsetWidth;
 
     aPg=document.querySelector('.activePage').style;
     aPg.left=contLeft;
     aPg.top=contTop;
-    aPg.borderRadius=w/2;
     aPg.width=w;
     aPg.height=w;
 })
