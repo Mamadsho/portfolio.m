431e4197873fc89a0173923fc56f63f3a5d79f92
Mamadsho Nazaramonov <mamadsho.nazaramonov@gmail.com>
2020-11-04 08:49:36 +0500

    toggles

--- concept/starterPage.js
+++ concept/starterPage.js
@@ -15,7 +15,32 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
+function toggleElement(el){
+    c1_bgColor=el.children[0].style.backgroundColor;
+    c1_boxShadow=el.children[0].style.boxShadow;
+    c2_bgColor=el.children[1].style.backgroundColor;
+    c2_boxShadow=el.children[1].style.boxShadow;
 
+    el.children[0].style.backgroundColor=c2_bgColor;
+    el.children[0].style.boxShadow=c2_boxShadow;
+    el.children[1].style.backgroundColor=c1_bgColor;
+    el.children[1].style.boxShadow=c1_boxShadow;
+
+}
+function toggle_lang(){
+    (lang!='ru')? lang='ru':lang='en';
+    toggleElement(document.querySelector('#lang'))
+}
+function toggle_fs(){
+    if (fs){
+        document.exitFullscreen()
+        fs=false;
+    }else{
+        document.body.requestFullscreen();
+        fs=true;
+    }
+    toggleElement(document.querySelector('#fs'))    
+}
 document.addEventListener('DOMContentLoaded',()=>{
     startTimer(4,document.querySelector('.timer'))
 })
