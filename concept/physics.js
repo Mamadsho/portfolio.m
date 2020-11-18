6ee94f381e82b62924de0a67e77031d494f016ac
Mamadsho Nazaramonov <mamadsho.nazaramonov@gmail.com>
2020-11-18 08:01:05 +0500

    removeExcessComputatoinComradesPhysX&HomePageLang

--- concept/physics.js
+++ concept/physics.js
@@ -1,4 +1,5 @@
 var trajectoryLength=0;
+var comrades=[];
 
 function startup(evt){
     document.querySelectorAll(".project").forEach(applyProjectListeners);
@@ -12,20 +13,21 @@ function applySliderListeners(sl){
 }
 
 function applyProjectListeners(el){
+    el.addEventListener('touchstart',tstartX)
     el.addEventListener('touchmove',tmoveX);
     el.addEventListener('touchend',tendX);
 }
-
+function tstartX(tevt){
+    const index = [...document.querySelector('.slider').querySelectorAll('.project')].indexOf(this);
+    const yLoop=document.querySelector('.slider').dataset.packSize*document.querySelector('.projectPack').querySelectorAll('.project').length;
+    const i=index%yLoop;
+    comrades=[pByN(i),pByN(i+yLoop),pByN(i+2*yLoop),pByN(i+3*yLoop)];
+}
 function tmoveX(tevt){
     let changedX=tevt.touches[0].clientX-prevTouchX.clientX;
     if (changedX)trajectoryLength+=Math.abs(changedX);//first time there's no prevTouch
     
     let l=getTranslateX(this);
-    
-    const index = [...document.querySelector('.slider').querySelectorAll('.project')].indexOf(this);
-    const yLoop=document.querySelector('.slider').dataset.packSize*document.querySelector('.projectPack').querySelectorAll('.project').length;
-    const i=index%yLoop;
-    const comrades=[pByN(i),pByN(i+yLoop),pByN(i+2*yLoop),pByN(i+3*yLoop)];
     comrades.forEach(function (cmr){
         cmr.style.transform=`translateX(${l+changedX}px)`;
     });
@@ -53,6 +55,7 @@ function tendX(tevt){
     //NULLING VARS
     //trajectoryLength=0; is nulled in positioning.js for click physX
     prevTouchX={};
+    comrades=[];
 }
 
 function tmoveY(tevt){
